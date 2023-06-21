771f1c5859b7ab781004b078a0b7b77ff506d650
drumfeet <drumfeet@gmail.com>
2023-06-21 20:39:18 +0800

    wip

--- script/addRelayJob.js
+++ script/addRelayJob.js
@@ -1,7 +1,6 @@
 import SDK_NODE from "weavedb-sdk-node"
 import fs from "fs"
 
-// const contractTxId = "XJFyEN-61isGjMX19E2uBMQ-kLFwOGE6RBFM5KoPQPI"
 const contractTxId = "e3RyM9TN4_8nJC_lPofpA3aWi6Zh_7rthom-aiWwBJw"
 
 const privateKeyFile = "wallet-mainnet.json"
@@ -9,9 +8,8 @@ const adminWallet = JSON.parse(fs.readFileSync(privateKeyFile).toString())
 
 const sdk = new SDK_NODE({
   contractTxId: contractTxId,
-  arweave_wallet: adminWallet,
 })
-sdk.initialize({ wallet: adminWallet })
+await sdk.initializeWithoutWallet()
 
 const job = {
   relayers: ["0x4e79fd1Ba59111b17817093AE36E812990A2634a"],
@@ -19,13 +17,6 @@ const job = {
       type: "number",
     },
 }
-
-const schema = {
-  type: "object",
-  required: ["height"],
-  properties: { height: { type: "number" } },
-}
-const relayers = ["0x4e79fd1Ba59111b17817093AE36E812990A2634a"]
 
 let result
 try {
