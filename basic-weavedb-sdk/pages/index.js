02e64355cd92ae85d60a8358ed8c9581cfa64fd0
drumfeet <drumfeet@gmail.com>
2023-07-18 23:59:33 +0800

    wip

--- basic-weavedb-sdk/pages/index.js
+++ basic-weavedb-sdk/pages/index.js
@@ -1,66 +1,6 @@
-import { useEffect, useState } from "react"
-import SDK from "weavedb-sdk"
+import Link from "next/link"
 
 export default function Home() {
-  //Replace contractTxId string value with the contractTxId when deploying the WeaveDB contract.
-  const contractTxId = "kdYSrsiN-UwPN9ID7oHF8SHuCKnUc8nHfvgyCozl5O4"
-  // Replace COLLECTION_NAME string value with the name of your collection. For this example, we have set people as our collection name.
-  const COLLECTION_NAME = "people"
-
-  // State variable storing an array of people data
-  const [people, setPeople] = useState([])
-  // State variable storing the weavedb-sdk object
-  const [db, setDb] = useState(null)
-  // State variable storing a boolean value indicating whether database initialization is complete.
-  const [initDb, setInitDb] = useState(false)
-
-  const setupWeaveDB = async () => {
-    try {
-      const _db = new SDK({
-        contractTxId: contractTxId,
-      })
-      await _db.init()
-      setDb(_db)
-      setInitDb(true)
-    } catch (e) {
-      console.error("setupWeaveDB", e)
-    }
-  }
-
-  // Function to retrieve all docs from the database collection.
-  const getCollection = async () => {
-    try {
-      const result = await db.cget(COLLECTION_NAME)
-      setPeople(result)
-      console.log("getCollection()", result)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  const handleAddClick = async () => {
-    const personData = { name: "Bob", age: Number(20) }
-
-    try {
-      const result = await db.add(personData, COLLECTION_NAME)
-      getCollection()
-      console.log("handleAddClick()", result)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  useEffect(() => {
-    setupWeaveDB()
-  }, [])
-
-  // Effect hook to retrieve all docs from the collection on database initialization.
-  useEffect(() => {
-    if (initDb) {
-      getCollection()
-    }
-  }, [initDb])
-
   return (
     <>
       <div
@@ -72,26 +12,15 @@ export default function Home() {
       >
         <br />
         <br />
-        <button onClick={handleAddClick}>Add Person</button>
-        <br /> <br />
-        <table cellPadding="8px">
-          <thead>
-            <tr align="left">
-              <th>Name</th>
-              <th>Age</th>
-              <th>DocId</th>
-            </tr>
-          </thead>
-          <tbody>
-            {people.map((item, index) => (
-              <tr key={index}>
-                <td>{item.data.name}</td>
-                <td>{item.data.age}</td>
-                <td>{item.id}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Link href="/add-person">add person</Link>
+
+        <br />
+        <br />
+        <Link href="/auto-sign">auto sign</Link>
+
+        <br />
+        <br />
+        <Link href="/prate11rr">prate11rr</Link>
       </div>
     </>
   )
