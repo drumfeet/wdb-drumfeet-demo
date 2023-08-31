053d0e0e046128b7c9b236115eeff7f0d4ddd87a
drumfeet <drumfeet@gmail.com>
2023-08-31 11:13:06 +0800

    wip

--- sdk-node/sample.js
+++ sdk-node/sample.js
@@ -1,35 +1,41 @@
 const WeaveDB = require("weavedb-sdk-node")
 require("dotenv").config()
 
-const contractTxId = "W00EhrAHqD2CxLd81N8yyCOOK0xtPTtfKIQEZGzKabU"
+const CONTRACT_TX_ID = "W00EhrAHqD2CxLd81N8yyCOOK0xtPTtfKIQEZGzKabU"
 const COLLECTION_NAME = "tasks"
 
-const evmWalletAddress = process.env.EVM_WALLET_ADDRESS
-const evmPrivateKey = process.env.EVM_PRIVATE_KEY
+const EVM_WALLET_ADDRESS = process.env.EVM_WALLET_ADDRESS.toLowerCase()
+const EVM_PRIVATE_KEY = Buffer.from(process.env.EVM_PRIVATE_KEY, "hex")
 
 const wallet = {
-  getAddressString: () => evmWalletAddress.toLowerCase(),
-  getPrivateKey: () => Buffer.from(evmPrivateKey, "hex"),
+  getAddressString: () => EVM_WALLET_ADDRESS,
+  getPrivateKey: () => EVM_PRIVATE_KEY,
+}
+
+async function addTask(db) {
+  const task = {
+    task: "Hey! I'm a task",
+    date: db.ts(),
+    user_address: db.signer(),
+    done: false,
+  }
+
+  const tx = await db.add(task, COLLECTION_NAME)
+  console.log("Transaction:", tx)
+
+  const result = await tx.getResult()
+  console.log("Result:", result)
 }
 
 async function main() {
   try {
-    const db = new WeaveDB({ contractTxId })
+    const db = new WeaveDB({ contractTxId: CONTRACT_TX_ID })
     await db.init()
     db.setDefaultWallet(wallet, "evm")
 
-    const task = {
-      task: "Hey! I'm a task",
-      date: db.ts(),
-      user_address: db.signer(),
-      done: false,
-    }
-    const tx = await db.add(task, COLLECTION_NAME)
-    console.log("tx", tx)
-    const result = await tx.getResult()
-    console.log("result", result)
-  } catch (e) {
-    console.error("catch", e)
+    await addTask(db)
+  } catch (error) {
+    console.error("An error occurred:", error)
   } finally {
     process.exit(0)
   }
