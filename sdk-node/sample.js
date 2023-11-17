f85d9f2cb4e04915302252b9e1886fd7ed1987bb
drumfeet <drumfeet@gmail.com>
2023-11-17 10:40:51 +0800

    wip

--- sdk-node/sample.js
+++ sdk-node/sample.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const CONTRACT_TX_ID = "W00EhrAHqD2CxLd81N8yyCOOK0xtPTtfKIQEZGzKabU"
 const COLLECTION_NAME = "tasks"
 
+// create .env file within the same folder if it does not already exist
 const EVM_WALLET_ADDRESS = process.env.EVM_WALLET_ADDRESS.toLowerCase()
 const EVM_PRIVATE_KEY = Buffer.from(process.env.EVM_PRIVATE_KEY, "hex")
 
@@ -21,17 +22,18 @@ async function addTask(db) {
   }
 
   const tx = await db.add(task, COLLECTION_NAME)
-  console.log("Transaction:", tx)
+  console.log("tx", tx)
 
   const result = await tx.getResult()
-  console.log("Result:", result)
+  console.log("result", result)
 }
 
 async function main() {
   try {
     const db = new WeaveDB({ contractTxId: CONTRACT_TX_ID })
     await db.init()
-    db.setDefaultWallet(wallet, "evm")
+
+    db.setDefaultWallet(wallet, "evm") // This wallet will be set as the default signer for all transactions
 
     await addTask(db)
   } catch (error) {
