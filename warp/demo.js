17052532b51276916123a0dcc1d9f0e206e4ef33
drumfeet <drumfeet@gmail.com>
2024-02-13 09:38:50 +0800

    add warp demo

---
+++ warp/demo.js
@@ -0,0 +1,20 @@
+const { WarpFactory } = require("warp-contracts")
+const fs = require("fs")
+
+async function main() {
+  const privateKeyFile = "wallet-mainnet.json"
+  const adminWallet = JSON.parse(fs.readFileSync(privateKeyFile).toString())
+
+  const warp = WarpFactory.forMainnet()
+  const contract = warp
+    .contract("fZ4eN2HJ-iGVPFDHZ9Vy7H5y8wtCSbmi8XXB0oBPQME")
+    .connect(adminWallet)
+    .setEvaluationOptions({
+      allowBigInt: true,
+    })
+  const { sortKey, cachedValue } = await contract.readState()
+  console.log("sortKey", sortKey)
+  console.log("cachedValue", cachedValue)
+}
+
+main()
